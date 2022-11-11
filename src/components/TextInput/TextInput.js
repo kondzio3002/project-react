65aec5a24c420eb5f6a5542f3772eb304bd8b8c8
Konrad Brożyna <kondzio3002@gmail.com>
2022-11-11 18:27:42 +0100

    Add ColumnForm

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,7 +1,7 @@
 import styles from './TextInput.module.scss';
 
 const TextInput = props => (
-  <input className={styles.input} placeholder={props.placeholder} type="text" />
+  <input className={styles.input} value={props.value} onChange={props.onChange} placeholder={props.placeholder} type="text" />
 );
 
 export default TextInput;
