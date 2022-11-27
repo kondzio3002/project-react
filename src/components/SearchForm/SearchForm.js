cc92a5290df8b10519a4105a97356cf3f074e12c
Konrad Brożyna <kondzio3002@gmail.com>
2022-11-27 21:04:52 +0100

    Add SearchString

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,28 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
-const SearchForm = () => (
-  <form className={styles.searchForm}>
-    <TextInput placeholder="Search..." />
-    <Button>
-      <span className="fa fa-search" />
-    </Button>
-  </form>
-);
+const SearchForm = () => {
+
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'UPDATE_SEARCHSTRING', payload: title });
+  };
+
+  return (
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <TextInput placeholder="Search..." value={title} onChange={e => setTitle(e.target.value)} />
+      <Button>
+        <span className="fa fa-search" />
+      </Button>
+    </form>
+  );
+};
 
 export default SearchForm;
