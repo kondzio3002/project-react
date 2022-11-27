2f151b2fadab9954fa735e0e7978d190d08bc3ac
Konrad Brożyna <kondzio3002@gmail.com>
2022-11-27 17:33:56 +0100

    Add initialState - edit List.js and Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,21 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
-const Column = props => (
-  <article className={styles.column}>
-    <h3 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h3>
-    <ul classNames={styles.cards}>
-      {props.cards.map(card => <Card key={card.id} title={card.title} />)}
-    </ul>
-    <CardForm columnId={props.id} action={props.action} />
-  </article>
-);
+const Column = props => {
+
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
+  return (
+    <article className={styles.column}>
+      <h3 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h3>
+      <ul classNames={styles.cards}>
+        {cards.map(card => <Card key={card.id} title={card.title} />)}
+      </ul>
+      <CardForm columnId={props.id} action={props.action} />
+    </article>
+  );
+};
 
 export default Column;
