a1ecc4a92b5a8e2f2edb23f27addaaadce62dbaf
Konrad Brożyna <kondzio3002@gmail.com>
2022-11-29 20:52:35 +0100

    Add PageTitle, About, Favorite and Home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Hero from './components/Hero/Hero';
-import SearchForm from './components/SearchForm/SearchForm';
-import List from './components/List/List';
+import Home from './components/Home/Home';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
+import Favorite from './components/Favorite/Favorite';
+import About from './components/About/About';
 
 const App = () => (
   <main>
     <NavBar />
     <Container>
-      <Hero />
-      <SearchForm />
-      <List />
+      <Home />
+      <About />
+      <Favorite />
     </Container>
   </main>
 );
