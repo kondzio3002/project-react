07058ccf520c565007553f10ca4e04b9fcb1061b
Konrad Brożyna <kondzio3002@gmail.com>
2022-11-29 22:17:11 +0100

    Add Link and NavLink

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import styles from './NavBar.module.scss';
 import Container from '../Container/Container';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -7,13 +8,16 @@ const NavBar = () => {
       <Container>
         <div className={styles.row}>
           <div className={styles.col}>
-            <a className={styles.icon} href="/"><i className="fa fa-tasks" /></a>
+            <Link className={styles.icon} to="/"><i className="fa fa-tasks" /></Link>
           </div>
           <div className={styles.col}>
             <ul className={styles.row}>
-              <li><a href="/">Home</a></li>
-              <li><a href="/favorite">Favorite</a></li>
-              <li><a href="/about">About</a></li>
+              <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} 
+              to="/">Home</NavLink></li>
+              <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} 
+              to="/favorite">Favorite</NavLink></li>
+              <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} 
+              to="/about">About</NavLink></li>
             </ul>
           </div>
         </div>
