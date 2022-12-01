a5e246e2e8ddea1f7a4b61bc07d4de95c3d8aa80
Konrad Brożyna <kondzio3002@gmail.com>
2022-12-01 20:46:54 +0100

    Add getColumnsByList and getListById

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -45,7 +45,8 @@ const initialState = {
     { id: 3, columnId: 2, title: 'Harry Potter' },
     { id: 4, columnId: 2, title: 'Star Wars' },
     { id: 5, columnId: 3, title: 'The Witcher' },
-    { id: 6, columnId: 3, title: 'Skyrim' }
+    { id: 6, columnId: 3, title: 'Skyrim' },
+    { id: 7, columnId: 4, title: 'Małe krople'}
   ],
   
   searchString: '',
